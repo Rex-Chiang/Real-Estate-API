e6b7cab6d2f8a16b875ec0e09f74ee39400c1747
Rex-Chiang <Rex.Chiang@luxshare-ict.com>
2021-11-07 17:57:53 +0800

    1. Update real estate data files. 2. Refactor data.py

--- RealEstateProject/RealEstateApp/utils/data.py
+++ RealEstateProject/RealEstateApp/utils/data.py
@@ -1,16 +1,26 @@
 from sqlalchemy import create_engine
 import pandas as pd
 
-df = pd.read_csv ("/Users/rex/Downloads/lvr_landAcsv/h_lvr_land_a.csv")
-df = df.drop([0])
-df = df.drop(columns = ["土地移轉總面積平方公尺", "都市土地使用分區", "非都市土地使用分區",
-                        "非都市土地使用編定", "主要用途", "主要建材",
-                        "建物現況格局-隔間", "車位類別", "備註",
-                        "編號", "主建物面積", "附屬建物面積",
-                        "陽台面積", "電梯", "移轉編號"])
+class RealEstateData:
+    def __init__(self):
+        self.df = pd.read_csv ("./real_estate_data_csv/h_lvr_land_a.csv")
 
-df["建物現況格局-房-廳-衛"] = df[["建物現況格局-房", "建物現況格局-廳", "建物現況格局-衛"]].apply('-'.join, axis = 1)
-df = df.drop(columns = ["建物現況格局-房", "建物現況格局-廳", "建物現況格局-衛"])
+    def data_processing(self):
+        self.df = self.df.drop([0])
+        self.df = self.df.drop(columns = ["土地移轉總面積平方公尺", "都市土地使用分區", "非都市土地使用分區",
+                                "非都市土地使用編定", "主要用途", "主要建材",
+                                "建物現況格局-隔間", "車位類別", "備註",
+                                "編號", "主建物面積", "附屬建物面積",
+                                "陽台面積", "電梯", "移轉編號"])
 
-engine = create_engine("mysql+pymysql://root:1209@localhost:3306/real_estate")
-df.to_sql(name = 'real_estate_taoyuan', con = engine, index = False)
+        self.df["建物現況格局-房-廳-衛"] = self.df[["建物現況格局-房", "建物現況格局-廳", "建物現況格局-衛"]].apply('-'.join, axis = 1)
+        self.df = self.df.drop(columns = ["建物現況格局-房", "建物現況格局-廳", "建物現況格局-衛"])
+
+    def df_to_database(self):
+        engine = create_engine("mysql+pymysql://root:1209@localhost:3306/real_estate")
+        self.df.to_sql(name = 'real_estate_taoyuan', con = engine, index = False)
+
+if __name__ == "__main__":
+    RealEstateData = RealEstateData()
+    RealEstateData.data_processing()
+    RealEstateData.df_to_database()
